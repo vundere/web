a5499504504b6074fe50276553b86f69c5285e13
Olav Hobberstad <o.hobberstad@stud.uis.no>
2017-02-15 10:40:16 +0100

    done?

--- Personal projects/bus/busmain.js
+++ Personal projects/bus/busmain.js
@@ -1,22 +1,21 @@
 var timeNow;
-var startLeaveX = moment('06:13', "HH:mm"); //random X60 timestamp
-// 13, 43
-var startLeave6 = moment('06:11', "HH:mm"); //random 6 timestamp
-// 11, 26, 41, 56
-var instanceX;
-var instance6;
 var x60times = [13,43];
 var sixTimes = [11,26,41,56];
 
-//fox' stuff?
+//fox' stuff
 function get_minutes_to_bus(bus_times) {
-  var currentMin = moment().minutes();
-  for (var i = 0; i < array.length; i++) {
-    array[i]
-  }
-}
+  var currentMin = timeNow.minutes();
+
+  for (bus_time in bus_times) {
+    if (bus_times[bus_time] > currentMin) {
+      return bus_times[bus_time] - currentMin;
+    };
+  };
+  return bus_times[0] + 60 - currentMin;
+};
 
 $(document).ready(function() {
+  //Fade stuff
   $('.innerR').mouseover(function(){
     $(this).closest('.innerR').fadeTo("fast",0.7);
   });
@@ -30,60 +29,16 @@ $(document).ready(function() {
     $(this).closest('.innerL').fadeTo("fast",1);
   });
 
-  var realTime = setInterval(function() {
+  // The meat of'er
+
+  var realTime = setInterval(function() { // Current time
     timeNow = moment();
-    $('#one').html(timeNow.format('HH:mm:ss'));
+    $('#two').html(timeNow.format('HH:mm:ss'));
     }, 100);
 
-  var x60time = setInterval(function() {
-    var ttime = 30;
-    instanceX = startLeaveX;
-    if (timeNow.hours() > instanceX.hours()) {
-      instanceX.add(1, 'hours');
-    };
-    if (timeNow.minutes() > 13 && timeNow.minutes() < 43 && instanceX.minutes() > 11 && instanceX.minutes() !== 43) {
-      instanceX.add(ttime, 'minutes');
-    } else if (timeNow.minutes() > 43 && instanceX.minutes() < 43 && instanceX.minutes() !== 13) {
-      instanceX.add(ttime, 'minutes');
-      instanceX.add(1, 'hours');
-    }
-    $('#two').html("X60: " +instanceX.format('HH:mm'));
-  }, 100);
-
-  var time6 = setInterval(function() {
-    var ttime = 15;
-    instance6 = startLeave6;
-    if (timeNow.hours() > instance6.hours()) {
-      instance6.add(1, 'hours');
-    };
-    if (26 > timeNow.minutes() > 11 && timeNow.minutes() < 26 && instance6.minutes() !== 26) {
-      instance6.add(ttime, 'minutes');
-    } else if (timeNow.minutes() > 26 && timeNow.minutes() < 41 && instance6.minutes() !== 41) {
-      instance6.add(ttime, 'minutes');
-    } else if (timeNow.minutes() > 41 && timeNow.minutes() < 56 && instance6.minutes() !== 56) {
-      instance6.add(ttime, 'minutes');
-    } else if (timeNow.minutes() > 56 && instance6.minutes() !== 11) {
-      instance6.add(ttime, 'minutes');
-      instance6.add(1, 'hours');
-    };
-    $('#four').html("6: " +instance6.format('HH:mm'));
-  }, 100);
-
-
   var nextBus = setInterval(function() {
-    var diff6 = instance6.minutes() - timeNow.minutes();
-    if (diff6 < 0) {
-      diff6 = 60 + diff6;
-    }
-    var diffX = instanceX.minutes() - timeNow.minutes();
-    if (diffX < 0) {
-      diffX = 60 + diffX;
-    }
-
-    if (diff6 < diffX) {
-      $('#three').html("Next: " +diff6);
-    } else {
-      $('#three').html("Next: " +diffX);
-    };
+    var busminX = get_minutes_to_bus(x60times);
+    var busmin6 = get_minutes_to_bus(sixTimes);
+    $('#three').html("X60: "+busminX +" 6: "+busmin6);
   }, 100);
 });
--- Personal projects/bus/foxpseudo.js
+++ Personal projects/bus/foxpseudo.js
@@ -4,12 +4,12 @@ def get_minutes_to_bus(bus_times):
 
   for bus_time in bus_times:
     if bus_time > current_minutes:
-      # bus arrives within the hour
+      // bus arrives within the hour
       return bus_time - current_minutes
 
   // bus arrives in the next hour
   return bus_times[0] + 60 - current_minutes
 
 // usage
-bus_x60_times = [13,43] # this array should be ordered
+bus_x60_times = [13,43] // this array should be ordered
 minutes_to_bus = get_minutes_to_bus(bus_x60_times)
